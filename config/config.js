a6927a72cb105c6894f04890ea11215faba7ead4
Nitish Kumar <nitishonspeed@gmail.com>
2017-12-09 17:21:09 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@
 Change the name myDb to your database name
 
 */
-var connectionstring = 'mongodb://localhost:27017/hacklife';//Change myDb to yourDBname
+//var connectionstring = 'mongodb://localhost:27017/hacklife';//Change myDb to yourDBname
 exports.serverOptions = {
      'auto_reconnect': true,
      'poolSize': 10
  };
-//var connectionstring = "mongodb://<dbuser>:<dbpassword>@ds135186.mlab.com:35186/hacklife";
+var connectionstring = "mongodb://<dbuser>:<dbpassword>@ds135186.mlab.com:35186/hacklife";
 exports.mongoconnectionstring = connectionstring;
